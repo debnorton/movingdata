8cd1db785130e829f5f8f90ea1a9aa5031850bb6
DeborahNorton <deb@dcodefm.com>
2022-06-01 14:36:07 -0700

    formatting

--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ let table;
 //https://datatables.net/examples/index
 
 window.loadTable = (json) => {
-  const columns = [];
-  const data = [];
+  const columns = [
+    {title: "First Name", data: "firstName", orderable: false},
+    {title: "Last Name", data: "lastName"},
+    {title: "Position", data: "position", visible: true}, 
+    {title: "Age", data: "age", searchable: false},
+  ];
+  const data = [
+    {firstName: "Deborah", lastName: "Norton", position: "Mom", age: "50"},
+    {firstName: "Justin", lastName: "Kertson", position: "Dad", age: "42"},
+    {firstName: "Reeve", lastName: "Bushman", position: "Son", age: "26"},
+    {firstName: "Odo", lastName: "Norton-Kertson", position: "Dog", age: "1"},
+    {firstName: "Dobby", lastName: "Norton-Kertson", position: "Cat", age: "2"},
+
+  ];
   table = $("#dtable").DataTable({
+    //paging: false,
+    //searching: false,
+    //ordering: false,
     columns: columns,
     data: data,
   });
