a2a7a5b15aea59042428e79a70828c2ace6a5a4f
DeborahNorton <deb@dcodefm.com>
2022-06-01 16:03:29 -0700

    tables

---
+++ src/columns.js
@@ -0,0 +1,11 @@
+export const customerColumns = [
+    {title: "CompanyName", data: "fieldData.CompanyName", orderable: true},
+    {title: "State", data: "fieldData.State"},
+    {title: "Zip", data: "fieldData.Zip"},
+  ];
+  export const admissionsColumns = [
+    {title: "Program", data: "fieldData.Program", orderable: true},
+    {title: "Category", data: "fieldData.Category"},
+    {title: "Major", data: "fieldData.Major", visible: true}, 
+    {title: "School", data: "fieldData.School", searchable: true},
+  ];
--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
+import { customerColumns, admissionsColumns} from "./columns";
+
 let table;
 
 //https://datatables.net/reference/option/
 //https://datatables.net/examples/index
 
 window.loadTable = (json) => {
-  const columns = [
-    {title: "First Name", data: "firstName", orderable: false},
-    {title: "Last Name", data: "lastName"},
-    {title: "Position", data: "position", visible: true}, 
-    {title: "Age", data: "age", searchable: false},
-  ];
-  const data = [
-    {firstName: "Deborah", lastName: "Norton", position: "Mom", age: "50"},
-    {firstName: "Justin", lastName: "Kertson", position: "Dad", age: "42"},
-    {firstName: "Reeve", lastName: "Bushman", position: "Son", age: "26"},
-    {firstName: "Odo", lastName: "Norton-Kertson", position: "Dog", age: "1"},
-    {firstName: "Dobby", lastName: "Norton-Kertson", position: "Cat", age: "2"},
+  const obj = JSON.parse(json);
+  const data = obj.data;
+  const source = obj.source;
+  
+  const columns = source ==="Customers" ? customerColumns: admissionsColumns;
 
-  ];
   table = $("#dtable").DataTable({
     //paging: false,
     //searching: false,
